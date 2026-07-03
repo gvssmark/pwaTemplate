eb4693088ba9d6bb5d1fdbd8ad96ed77932067a9
gvssmark <83894487+gvssmark@users.noreply.github.com>
2026-07-04 04:00:38 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const APP = {
+    NAME: "My Application",
+    SHORT_NAME: "MyApp",
+    VERSION: "1.0.0",
+
+    CACHE: "myapp-v1.0.0",
+
+    GAS_URL: "",
+
+    AUTHOR: "GVSS Mark",
+
+    THEME_COLOR: "#1565C0",
+
+    BACKGROUND_COLOR: "#ffffff"
+};
